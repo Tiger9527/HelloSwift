c155c936c96a035e369878acd34dcc16b6a81bc5
tiger <465928180@qq.com>
2018-04-13 18:16:22 +0800

    新增 uicolor extension

---
+++ HelloSwift/HelloSwift/UIColor+hexColor.swift
@@ -0,0 +1,40 @@
+//
+//  UIColor+hexColor.swift
+//  HelloSwift
+//
+//  Created by zhaotai on 2018/4/13.
+//  Copyright © 2018年 zhaotai. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIColor {
+    convenience init(_ r: CGFloat, _ g: CGFloat, _ b: CGFloat, _ a: CGFloat) {
+        self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
+    }
+
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
+    
+    static func colorWithHex(hexColor:Int64)->UIColor{
+        let red = ((CGFloat)((hexColor & 0xFF0000) >> 16))/255.0;
+        let green = ((CGFloat)((hexColor & 0xFF00) >> 8))/255.0;
+        let blue = ((CGFloat)(hexColor & 0xFF))/255.0;
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+}
